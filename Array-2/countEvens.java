ab1e92996af1fea92f6f92219669a2d96ee94b4f
Thomas Johannes <tjohannes.ee@gmail.com>
2017-09-06 21:22:11 -0700

    Initial commit

---
+++ Array-2/countEvens.java
@@ -0,0 +1,17 @@
+/* Return the number of even ints in the given array.
+ */
+ 
+// countEvens([2, 1, 2, 3, 4]) → 3
+// countEvens([2, 2, 0]) → 3
+// countEvens([1, 3, 5]) → 0
+
+
+public int countEvens(int[] nums) {
+  int count = 0;
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 == 0) {
+      count++;
+    }
+  }
+  return count;
+}
